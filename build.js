2e8cfe796708f77c9345a8bae96d7a2a9a27032e
lfkdsk <liufengkai@bytedance.com>
2025-01-13 19:50:19 -0800

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-   const browser = await puppeteer.launch({headless: 'new'});
+   const browser = await puppeteer.launch({
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox']      
+   });
    const page = await browser.newPage();
    await page.setViewport({
       width: 2560,
